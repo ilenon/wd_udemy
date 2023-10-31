5165701576b8c62be2d6729917bb6f11947b5c18
Ilenon <ilenon@MacBook-Air-de-Ivan.local>
2023-10-30 22:08:34 -0300

    First Commit WP project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <?php wp_head(); ?>
+  <title>Document</title>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+
+  <h1>Olá mundo!</h1>
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
